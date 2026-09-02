3858774c23d918569b64f4541b3a08dc94ee42b5
Jamesbond00726 <singh007namanbhai@gmail.com>
2026-09-03 00:16:10 +0530

    Add: relative-sort-array - submission-1

---
+++ Data Structures & Algorithms/relative-sort-array/submission-1.java
@@ -0,0 +1,36 @@
+class Solution {
+    public int[] relativeSortArray(int[] arr1, int[] arr2) {
+        int[] ans=new int[arr1.length];
+        Map<Integer,Integer> map=new HashMap<>();
+        for (int num: arr1){
+            map.put(num,map.getOrDefault(num,0)+1);
+        }
+        int[] freq=new int[1000];
+        for (int j : arr1) {
+            freq[j] = map.get(j);
+        }
+        int ind=0;
+        for (int j : arr2) {
+            int n = freq[j];
+            int prev=ind;
+            while (ind < prev + n) {
+                ans[ind] = j;
+                ind++;
+                freq[j]--;
+            }
+        }
+        if (ind < arr1.length) {
+            for (int i = 0; i < freq.length; i++) {
+                if (freq[i] > 0) {
+                    int prev=ind;
+                    while (ind< prev +freq[i]){
+                        ans[ind]=i;
+                        ind++;
+                    }
+                    freq[i]--;
+                }
+            }
+        }
+        return ans;
+    }
+}
